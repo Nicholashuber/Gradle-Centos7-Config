77064ca3d50b033a49782bcc1d571127a98f17f8
Nicholas Huber <Nick@huber.dev>
2020-07-18 23:40:09 -0400

    Create gradle.sh

---
+++ gradle.sh
@@ -0,0 +1,6 @@
+##What they tell you to use
+##export GRADLE_HOME=/opt/gradle/gradle-6.4.1
+##export PATH=${GRADLE_HOME}/bin:${PATH
+##
+##What you should actually use
+export PATH=${PATH}:/opt/gradle/gradle-6.4.1/bin
